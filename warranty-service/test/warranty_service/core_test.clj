755eec4c1682494c5f51185efe05589436fd61eb
Никита Иксарица <xomav2@gmail.com>
2020-11-30 20:43:57 +0300

    Создан пустой проект warranty-service

---
+++ warranty-service/test/warranty_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns warranty-service.core-test
+  (:require [clojure.test :refer :all]
+            [warranty-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
