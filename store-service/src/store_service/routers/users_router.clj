e5e59f89e8792d7e69d2e7d285ebb89cb86c77bb
Никита Иксарица <xomav2@gmail.com>
2021-01-21 08:45:18 +0300

    кажется так лучше читается

--- store-service/src/store_service/routers/users_router.clj
+++ store-service/src/store_service/routers/users_router.clj
@@ -18,33 +18,29 @@
 
 (s/def ::request-warranty (s/keys :req-un [::reason]))
 
-(def ^:private uuid-pattern
-  #"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
-(s/def ::uuid (s/and string? #(re-matches uuid-pattern %)))
-
 (defroutes routes
   (context "/api/v1/store/:user-uid" {{user-uid :user-uid} :params
                                       {{claims-uid :user-uid} :claims} :params}
     (GET "/orders" []
-      (validate-and-handle #(service/find-user-orders! (as-uuid claims-uid) (as-uuid %))
-                           [::uuid user-uid]))
+      (validate-and-handle #(service/find-user-orders! (as-uuid claims-uid) %)
+                           [uuid? (as-uuid user-uid)]))
     (POST "/purchase" {:keys [body]}
-      (validate-and-handle #(service/make-purchase! (as-uuid claims-uid) (as-uuid %) %2)
-                           [::uuid user-uid]
+      (validate-and-handle #(service/make-purchase! (as-uuid claims-uid) % %2)
+                           [uuid? (as-uuid user-uid)]
                            [::purchase-item body]))
     (context "/:order-uid" [order-uid :<< as-uuid]
       (GET "/" []
-        (validate-and-handle #(service/find-user-order! (as-uuid claims-uid) (as-uuid %) %2)
-                             [::uuid user-uid]
+        (validate-and-handle #(service/find-user-order! (as-uuid claims-uid) % %2)
+                             [uuid? (as-uuid user-uid)]
                              [uuid? order-uid]))
       (POST "/warranty" {:keys [body]}
-        (validate-and-handle #(service/warranty-request! (as-uuid claims-uid) (as-uuid %) %2 %3)
-                             [::uuid user-uid]
+        (validate-and-handle #(service/warranty-request! (as-uuid claims-uid) % %2 %3)
+                             [uuid? (as-uuid user-uid)]
                              [uuid? order-uid]
                              [::request-warranty body]))
       (DELETE "/refund" []
-        (validate-and-handle  #(service/refund-purchase! (as-uuid claims-uid) (as-uuid %) %2)
-                              [::uuid user-uid]
+        (validate-and-handle  #(service/refund-purchase! (as-uuid claims-uid) % %2)
+                              [uuid? (as-uuid user-uid)]
                               [uuid? order-uid]))))
   (route/not-found {:status 404}))
 
