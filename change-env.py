6dbd9221eea77bde72deb128dd5723068714b1cb
Никита Иксарица <xomav2@gmail.com>
2021-01-21 21:28:07 +0300

    Скрипт для игры с Docker'ом

--- change-env.py
+++ change-env.py
@@ -1,24 +1,49 @@
 import sys
 import os
 
-def func(old, new):
+def replace_text_in_file(path, old, new):
+    filedata = None
+        
+    with open(path, 'r') as file:
+        filedata = file.read()
+
+    filedata = filedata.replace(old, new)
+
+    with open(path, 'w') as file:
+        file.write(filedata)
+
+def change_env(to_local):
     services = []
+    port_map = {'warranty': '8180',
+                'warehouse': '8280',
+                'order': '8380',
+                'store': '8480',
+                'session': '8580'}
+    port_map = {} # Чтобы не трогать Dockerfile'ы.
+    local_env = ':env-type :local'
+    heroku_env = ':env-type :heroku'
     
     for filename in os.listdir('.'):
         if os.path.isdir(filename) and filename.endswith('-service'):
             services.append(filename)
             
     for service_folder in services:
-        filedata = None
-        
-        with open(f'./{service_folder}/config.edn', 'r') as file:
-          filedata = file.read()
+        congig_path = f'./{service_folder}/config.edn'
+        dockerfile_path = f'./{service_folder}/Dockerfile'
+        service = service_folder.replace('-service', '')
 
-        filedata = filedata.replace(old, new)
+        if (to_local):
+            replace_text_in_file(congig_path, heroku_env, local_env)
 
-        with open(f'./{service_folder}/config.edn', 'w') as file:
-          file.write(filedata)
+            if (service in port_map):
+                replace_text_in_file(dockerfile_path, 'CMD', f'EXPOSE {port_map[service]}\nCMD')
+                replace_text_in_file(dockerfile_path, '0.0.0.0 $PORT', f'127.0.0.1 {port_map[service]}')
+        else:
+            replace_text_in_file(congig_path, local_env, heroku_env)
             
+            if (service in port_map):
+                replace_text_in_file(dockerfile_path, f'EXPOSE {port_map[service]}\nCMD', 'CMD')
+                replace_text_in_file(dockerfile_path, f'127.0.0.1 {port_map[service]}', '0.0.0.0 $PORT')
 
 if __name__ == '__main__':
     if (len(sys.argv[1:]) == 0):
@@ -29,10 +54,10 @@ if __name__ == '__main__':
         heroku_env = ':env-type :heroku'
         
         if (env_type == 'local' or env_type == 'l'):
-            func(heroku_env, local_env)
+            change_env(True)
             print('Local environment done.')
         elif (env_type == 'heroku' or env_type == 'h'):
-            func(local_env, heroku_env)
+            change_env(False)
             print('Heroku environment done.')
         else:
             print('кря')
